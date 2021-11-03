32469f68e154e0a131453048c60b1e4c5a2f6d5e
Elizabeth Joseph <92835477+ElizabethJoseph25@users.noreply.github.com>
2021-11-03 12:07:01 +0530

    updating tinyProject.java
    added some functions

--- tinyproject.java
+++ tinyproject.java
@@ -1,30 +1,34 @@
 import java.util.Scanner;
 
-public class tinyproject {
-    public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("WELCOME TO SIMELABS ");
+public class tinyproject 
+{
+    int age,yearOfPassout;
+    double cgpa;
+    Scanner scanner = new Scanner(System.in);
+    public void getDetails()
+    {
         System.out.print("Enter your Name: ");
-        String name=scanner.next();
+        String name = scanner.next();
         System.out.print("Enter Your age: ");
-        int age = scanner.nextInt();
+        age = scanner.nextInt();
         System.out.println("Enter your Qualification: ");
-        String Qualfication=scanner.next();
+        String Qualfication = scanner.next();
         System.out.print("Enter your College Name: ");
-        String collegename=scanner.next();
+        String collegename = scanner.next();
         System.out.println("enter your stream");
         String stream = scanner.next();
         System.out.println("enter you cgpa");
-        double cgpa = scanner.nextDouble();
+        cgpa = scanner.nextDouble();
         System.out.println("Enter year of passout: ");
-        int yearOfPassout= scanner.nextInt(); 
-        
-        if(yearOfPassout==2021 && cgpa>=7)
-        {
-        	System.out.println("You are eligible to attend Interview");
-        }else {
-        	System.out.println("You are not eligible to attend Interview");
-     }
-        
+        yearOfPassout = scanner.nextInt();
+    }
+    public void isEligible()
+    {
+
+        if (yearOfPassout == 2021 && cgpa >= 7) {
+            System.out.println("You are eligible to attend Interview");
+        } else {
+            System.out.println("You are not eligible to attend Interview");
+        }
     }
 }
